4b11075cb51674610ec2ff7c1bf1db306d6dc3db
matd127 <mateusz333555@gmail.com>
2022-05-23 18:29:38 +0200

    register, login

--- index.ts
+++ index.ts
@@ -5,10 +5,12 @@ import {connect} from './db'
 const paymentmetods = require('./routers/paymentmethod')
 const departurecities = require('./routers/departurecity')
 const hotels = require('./routers/hotel')
+const users = require('./routers/user')
 const app = express()
 app.use(express.json())
 connect();
 
+app.use('/users', users)
 app.use('/hotels', hotels)
 app.use('/departurecities', departurecities)
 app.use('/paymentmethods', paymentmetods)
